65045fd15323e7afda2abc84bef5713be0da83cd
Aashish Kumar Ray <2002raysi@gmail.com>
2025-01-03 20:07:08 +0530

    Scope-Block-Shadowing

---
+++ Desktop/WEB_DEV/JavaScriptAdvance/block-scope-shadowing/app.js
@@ -0,0 +1,22 @@
+// // Block: Anything inside curly brackets are call Block
+// {
+//     let nam = "Aashish";
+//     console.log(nam)
+// }
+// let nam = "Kanxu";
+// console.log(nam);
+
+// //Shadowing
+// let x = 116;
+// function inner(){
+//     let x = 161;
+//     console.log(`inner x: ${x}`); // WIll give 161
+// }
+// inner();
+// console.log(`Outer x : ${x}`); // Will give 116
+
+
+//Scope ---> We already knows that there are three scopes
+// 1. Global Scope
+// 2. Functional Scope 
+// 3. Block Scope
