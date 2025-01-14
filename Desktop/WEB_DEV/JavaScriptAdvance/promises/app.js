dd7661bd94a22bafddcd1f82134ce058785a2462
Aashish Kumar Ray <2002raysi@gmail.com>
2025-01-14 15:34:49 +0530

    Promiese

--- Desktop/WEB_DEV/JavaScriptAdvance/promises/app.js
+++ Desktop/WEB_DEV/JavaScriptAdvance/promises/app.js
@@ -1,34 +1,49 @@
-const promiseOne = new Promise((resolve, reject) => {
-    const success = true;
-    setTimeout(() => {
-        if(success){
-            console.log("SUCCESS");
-            resolve();
-        }
-        else{
-            console.log("FAILED");
-            reject();
-        }
-    }, 1000)
-})
+// const promiseOne = new Promise((resolve, reject) => {
+//     const success = true;
+//     setTimeout(() => {
+//         if(success){
+//             console.log("SUCCESS");
+//             resolve();
+//         }
+//         else{
+//             console.log("FAILED");
+//             reject();
+//         }
+//     }, 1000)
+// })
 
-promiseOne
-    .then(() => {
-        console.log("Then is exectued");
-    })
-    .catch((error) => console.error(error));
+// promiseOne
+//     .then(() => {
+//         console.log("Then is exectued");
+//     })
+//     .catch((error) => console.error(error));
     
 
 
-new Promise((resolve, reject) => {
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("Async task is done");
+//         resolve();
+//     }, 500)
+// })
+//     .then(() => {
+//         console.log("Promise is exectued");
+//     })
+//     .catch((error) => {
+//         console.error(error);
+//     })
+
+const promiseThree = new Promise((resolve, reject) => {
+    const user = {
+        username : "Preeti",
+        email : "2004preeti@gmail.com"
+    }
     setTimeout(() => {
-        console.log("Async task is done");
-        resolve();
-    }, 500)
+        resolve(user);
+    }, 1000);
 })
-    .then(() => {
-        console.log("Promise is exectued");
-    })
-    .catch((error) => {
-        console.error(error);
+
+promiseThree
+    .then((user) => {
+        console.log(user);
     })
